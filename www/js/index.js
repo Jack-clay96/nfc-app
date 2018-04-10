767f58f308e5fc02f0d52f7521892002578e759f
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2018-04-10 17:08:51 +0100

    update

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,4 @@
 /*Backendless: https://backendless.com/docs/js/doc.html#welcome*/
-
 Backendless.initApp("3DCEF922-7E9F-7195-FF0F-9D3ECB207C00","F13A43EA-ED3B-2C2A-FF44-80306BEA1A00"); //AppID then JS API key
 document.addEventListener("deviceready", onDeviceReady, false);
 
@@ -14,13 +13,29 @@ function updateDisplay() {
 	
 	console.log("device ready!");
     
-    /* NFC SCANNING*/
-    nfc.addNdefListener(callback, [onSuccess], [onFailure]); //TEST ON ACTUAL DEVICE. nfc not defined.
+   // Read NDEF formatted NFC Tags
+    nfc.addNdefListener (
+        function (nfcEvent) {
+            var tag = nfcEvent.tag,
+                ndefMessage = tag.ndefMessage;
+
+            // dump the raw json of the message
+            // note: real code will need to decode
+            // the payload from each record
+            alert(JSON.stringify(ndefMessage));
+
+            // assuming the first record in the message has
+            // a payload that can be converted to a string.
+            alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+        },
+        function () { // success callback
+            alert("Waiting for NDEF tag");
+        },
+        function (error) { // error callback
+            alert("Error adding NDEF listener " + JSON.stringify(error));
+        }
+    );
+},
         
 }
 
-    function gotError( err ) // see more on error handling
-    {
-        console.log( "error message - " + err.message );
-        console.log( "error code - " + err.statusCode );
-    }
