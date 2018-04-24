8cf1e29b1dcbb792cf55977810e1e63f3a5730bc
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2018-04-24 16:19:50 +0100

    u[date

--- www/js/index.js
+++ www/js/index.js
@@ -14,6 +14,69 @@ function updateDisplay() {
     function onDeviceReady() {
 	
 	console.log("device ready!");
+        
+     /*Login Page */
+     $( "#loginButton" ).click(function() {
+        
+        console.log("button clicked")
+         
+        var username = $('#email').val();
+        var password = $('#password').val();
+        
+        function userLoggedIn( user )
+        {
+            console.log( "user has logged in" + user);
+            location.href="#homePage";
+        }
+ 
+        console.log( username + ", " + password );
+         Backendless.UserService.login( username, password, true )
+            .then( userLoggedIn )
+            .catch( gotError );
+     });
+        
+        /*Register Page */
+    $( "#register" ).click(function() {
+        console.log("button clicked")
+        var email = $("#emailReg").val();
+        var name = $("#nameReg").val();
+        var password = $("#passwordReg").val();
+        
+    function userRegistered( user )
+        {
+            console.log( "user has been registered" );
+            location.href="#loginPage";
+        }
+ 
+        var user = new Backendless.User();
+        user.email = email;
+        user.name = name;
+        user.password = password;
+ 
+        Backendless.UserService.register( user ).then( userRegistered ).catch( gotError );
+        
+    });
+        
+        
+    /* Forgot PWD Page */
+    $("#ForgpassBut").click(function(){
+        console.log("forgot password button clicked");
+        var emailRestPwd= $("#emailPass").val(); //user email here
+            
+        function passwordRecoverySent()
+        {
+            console.log( "an email with a link to restore password has been sent to the user" );
+            location.href="#loginPage";
+        }
+
+         Backendless.UserService.restorePassword(emailRestPwd)
+         .then( passwordRecoverySent )
+        .catch( gotError );
+            
+        });
+        
+    /*Home Page */
+    $('#myPanel').enhanceWithin().panel();
     
 // Read NDEF formatted NFC Tags
     $( "#read" ).click(function() {
@@ -52,3 +115,9 @@ function updateDisplay() {
     updateDisplay();
     });
 }
+
+    function gotError( err ) // see more on error handling
+    {
+        console.log( "error message - " + err.message );
+        console.log( "error code - " + err.statusCode );
+    }
