b63559befc0a0b4118e059b97a5b61001d7fda10
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2018-04-29 19:38:45 +0100

    test agn

--- www/js/index.js
+++ www/js/index.js
@@ -141,7 +141,7 @@ function processResults(productInfo) {
     Backendless.Data.of( "productInfo" ).findById( this.id )
     .then( function( result ) {
     //data about event here
-    console.log("result: "+result);
+    console.log(this.id.ProductName);
     $("#headerPartName").append(this.id.ProductName);
     })
     .catch( function( error ) {
