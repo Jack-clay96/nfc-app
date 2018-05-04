23a7175cfa16e4b84b09ffeb397b2361dd4e5d59
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2018-05-04 14:22:45 +0100

    id write

--- www/js/index.js
+++ www/js/index.js
@@ -118,7 +118,7 @@ $(document).on("pageshow","#settingsPage", onSettingPageShow);
 //Write implementation
         function writeTag (nfcEvent) {
             console.log("writeTag function ran");
-            message = [ndef.textRecord("New Product Name"), ndef.textRecord("5")];
+            message = [ndef.textRecord("Product Name:" + "#writeProduct" + "Quantity: " + "#writeQuantity")];
             nfc.write(message);
             console.log(message);
         }
